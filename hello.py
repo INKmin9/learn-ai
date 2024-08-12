6302dd581c3f0196df5b80a67d6c1083f06e3066
INKmin9 <mkkim9901@naver.com>
2024-08-12 16:26:34 +0900

    format method

--- hello.py
+++ hello.py
@@ -1,8 +1,1 @@
-burger_price = 4990
-fries_price = 1490
-beverage_price = 1250
-
-print(burger_price)
-print(burger_price * 2)
-print(burger_price + fries_price)
-print(burger_price * 3 + fries_price * 2 + beverage_price * 5)
+print(2 ** 3.0)

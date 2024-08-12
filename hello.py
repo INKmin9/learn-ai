3578a928e38cad0737f173373eec7ca72459c78b
INKmin9 <mkkim9901@naver.com>
2024-08-12 15:31:01 +0900

    파이썬 너무 어색해요

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+burger_price = 4990
+fries_price = 1490
+beverage_price = 1250
+
+print(burger_price)
+print(burger_price * 2)
+print(burger_price + fries_price)
+print(burger_price * 3 + fries_price * 2 + beverage_price * 5)

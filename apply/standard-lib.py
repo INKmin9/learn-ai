c5929dca8b1e8fc63776872b51429ea8df909352
INKmin9 <mkkim9901@naver.com>
2024-08-14 15:43:58 +0900

    python apply - standard library

---
+++ apply/standard-lib.py
@@ -0,0 +1,11 @@
+import math
+print(math.log10(100))
+print(math.cos(0))
+print(math.pi)
+
+import random
+print(random.random()) #0.0~1.0 사이 랜덤한 값 뽑아오기
+
+import os
+print(os.getlogin())
+print(os.getcwd())

dec73c9c2cd5efdd7efaf42dfcf282da09767110
INKmin9 <mkkim9901@naver.com>
2024-08-14 14:21:09 +0900

    python advanced - fahrenheit to celsius

---
+++ advanced/list-indexing.py
@@ -0,0 +1,9 @@
+# greetings 리스트의 원소를 모두 출력하는 프로그램을 작성해 보세요. while문과 리스트의 개념을 활용하시면 됩니다.
+
+greetings = ["안녕", "니하오", "곤니찌와", "올라", "싸와디캅", "헬로", "봉주르"]
+
+# 여기에 코드를 작성하세요
+i = 0
+while i < len(greetings):
+    print(greetings[i])
+    i += 1

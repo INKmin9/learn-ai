7e98ec9b3e8fcf5b1f47a22ce2fb66a89aa00153
INKmin9 <mkkim9901@naver.com>
2024-08-14 12:24:35 +0900

    python basics - function

--- basic/definition.py
+++ basic/definition.py
@@ -9,3 +9,14 @@ def cafe_mocha_recipe():
 # 테스트 코드
 cafe_mocha_recipe()
 cafe_mocha_recipe()
+
+
+# 코드를 작성하세요.
+def multiply_three_numbers(a, b, c):
+    print(a*b*c)
+
+
+# 테스트 코드
+multiply_three_numbers(7, 3, 5)
+multiply_three_numbers(21, 4, 9)
+multiply_three_numbers(-7, 6, 3)

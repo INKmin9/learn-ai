b9264d8c36155ef27bab6719e47b011be7012b4d
INKmin9 <mkkim9901@naver.com>
2024-08-14 14:16:04 +0900

    delete

--- boolean.py
+++
@@ -1,7 +0,0 @@
-print(True and False)
-print(True or True)
-print(False and True)
-print(False or False)
-
-print(not True)
-print(not False)
--- type-function.py
+++
@@ -1,7 +0,0 @@
-print(type(3))
-print(type(3.0))
-print(type("3"))
-
-print(type("True"))
-
-

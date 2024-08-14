bfda4cd081cceeaa00e6aa790213d35e58d6d8d8
INKmin9 <mkkim9901@naver.com>
2024-08-14 10:49:50 +0900

    for loop

---
+++ for-loop.py
@@ -0,0 +1,13 @@
+my_list=[2, 3, 5, 7, 11]
+for number in my_list:
+    print(number)
+
+# range 함수
+for i in range(3, 11): # 3부터 10까지 출력하는 함수
+    print(i)
+
+for i in range(9): # 0부터 8까지 출력하는 함수
+    print(i)
+
+for i in range(3, 17, 3): # start, stop, step 간격을 준다. 등차수열이라 생각하면 된다.
+    print(i)

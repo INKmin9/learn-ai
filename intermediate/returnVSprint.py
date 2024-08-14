624d005244c3efd2f7c5db670fb07470c0bea5b2
INKmin9 <mkkim9901@naver.com>
2024-08-14 13:47:30 +0900

    python intermediate - difference btw return and print

---
+++ intermediate/returnVSprint.py
@@ -0,0 +1,21 @@
+def first():
+    message = "코드잇"
+    return message
+
+
+def second():
+    message = "codeit"
+    return message
+
+
+def third():
+    message = None
+    return message
+
+
+
+# 테스트 코드
+print(first())
+print(second())
+print(third())
+print(third())

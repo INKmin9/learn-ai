5fb87a33cc6ff783ff3ec147928d9d9dcd909562
INKmin9 <mkkim9901@naver.com>
2024-08-14 15:05:45 +0900

    python advanced - dictionary vocab

---
+++ advanced/vocab.py
@@ -0,0 +1,14 @@
+# 1. 단어장 만들기
+vocab = {
+    'sanitizer': '살균제',
+    'ambition': '야망',
+    'conscience': '양심',
+    'civilization': '문명'
+}
+print(vocab)
+
+vocab['privilege'] = '특권'
+vocab['principle'] = '원칙'
+# 2. 새로운 단어들 추가
+# 여기에 코드를 작성하세요
+print(vocab)
